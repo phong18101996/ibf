f737578d66628fcca139e6ff33fafbe091b04fcc
phongnguyendev <phong.nguyen@fractal.vn>
2022-09-19 10:38:05 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 1000,
+        // autoplay: true,
+        // autoplaySpeed: 1000,
     });
 
     // $('.slider-item').slick({
