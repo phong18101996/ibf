475a41295386fd31ba30e16b556dbd3b76dfd926
phongnguyendev <phong.nguyen@fractal.vn>
2022-09-21 15:07:03 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,10 @@ $(document).ready(function() {
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
     });
-
-    // $('.slider-item').slick({
-    //     prevArrow: false,
-    //     nextArrow: false,
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 300,
-    //     slidesToShow: 1,
-    //     adaptiveHeight: true,
+    // $(window).on('load', function() {
+    //     $('#exampleModal').modal('show');
     // });
 });
