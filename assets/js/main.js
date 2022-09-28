26d3ad2af089de031381022c8e2777e38754927e
phongnguyendev <phong.nguyen@fractal.vn>
2022-09-28 15:52:55 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,14 +21,81 @@ $(document).ready(function() {
 
     // form subsribe
 
-    // $(window).on('load', function() {
-    //     $('#exampleModal').modal('show');
-    // });
+    $(window).on('load', function() {
+        $('#exampleModal').modal('show');
+    });
 
     // form thanks you
     
     // $(window).on('load', function() {
     //     $('#thanks-form').modal('show');
     // });
-   
+
+
+});
+// stacked card
+// $(document).ready(function() { 
+//     var $card = $('.ibf-card');
+//     var lastCard = $(".card-list .ibf-card").length - 1;
+
+// $('.next').click(function(){ 
+//     var $card = $('.ibf-card');
+//     var lastCard = $(".card-list .ibf-card").length - 1;
+// 	var prependList = function() {
+// 		if( $('.ibf-card').hasClass('activeNow') ) {
+// 			var $slicedCard = $('.ibf-card').slice(lastCard).removeClass('transformThis activeNow');
+// 			$('.item').prepend($slicedCard);
+// 		}
+//         var $slicedCard = $('.ibf-card').slice(lastCard).removeClass('transformThis activeNow');
+// 	}
+// 	$('.item').last().removeClass('transformPrev').addClass('transformThis').prev().addClass('activeNow');
+// 	setTimeout(function(){prependList(); }, 150);
+// });
+
+// $('.prev').click(function() {
+//     var $card = $('.ibf-card');
+//     var lastCard = $(".card-list .ibf-card").length - 1;
+// 	var appendToList = function() {
+// 		if( $('.ibf-card').hasClass('activeNow') ) {
+// 			var $slicedCard = $('.ibf-card').slice(0, 1).addClass('transformPrev');
+// 			$('.card-list').append($slicedCard);
+// 		}}
+	
+// 			$('.item').removeClass('transformPrev').last().addClass('activeNow').prevAll().removeClass('activeNow');
+// 	setTimeout(function(){appendToList();}, 150);
+// });
+// });
+
+
+
+$(document).ready(function() { 
+    var $card = $('.ibf-card');
+    var lastCard = $(".card-list .ibf-card").length - 1;
+
+$('.next').click(function(){ 
+    var $card = $('.ibf-card');
+    var lastCard = $(".card-list .ibf-card").length - 1;
+	var prependList = function() {
+		if( $('.ibf-card').hasClass('activeNow') ) {
+			var $slicedCard = $('.ibf-card').slice(lastCard).removeClass('transformThis activeNow');
+			$('.ibf-card').prepend($slicedCard);
+		}
+        var $slicedCard = $('.ibf-card').slice(lastCard).removeClass('transformThis activeNow');
+	}
+	$('.ibf-card').last().removeClass('transformPrev').addClass('transformThis').prev().addClass('activeNow');
+	setTimeout(function(){prependList(); }, 150);
+});
+
+$('.prev').click(function() {
+    var $card = $('.ibf-card');
+    var lastCard = $(".card-list .ibf-card").length - 1;
+	var appendToList = function() {
+		if( $('.ibf-card').hasClass('activeNow') ) {
+			var $slicedCard = $('.ibf-card').slice(0, 1).addClass('transformPrev');
+			$('.card-list').append($slicedCard);
+		}}
+	
+			$('.ibf-card').removeClass('transformPrev').last().addClass('activeNow').prevAll().removeClass('activeNow');
+	setTimeout(function(){appendToList();}, 150);
+});
 });
